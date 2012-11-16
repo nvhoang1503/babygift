42f529a12652a44fb42edbf7e9698a91a542d33c
hoang.nguyen <hoang.nguyen@techpropulsionlabs.com>
2012-11-16 15:35:25 +0700

    first all commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Littlespark::Application.config.secret_token = '8fce807908da9cbb598a4e80319d631ec2b30c1b65eb6c65bd836bb3456e3b76fe7bd93202749b64afc1f018c7598a608b682ab2adf5363bf6001ccf1233349e'
