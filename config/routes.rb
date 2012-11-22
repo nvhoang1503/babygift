a9e6772cb68be698c7a8e6a46dee94dcc8ef4bed
Phuong Doan <phuong.doan@techpropulsionlabs.com>
2012-11-22 15:37:25 +0700

    [styling] kits page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Littlespark::Application.routes.draw do
+  root :to => 'home#index'
+
   get "home/index"
   get "home/how_it_work"
   get "home/fqa"
   get "home/contact_us"
   get "home/about_us"
-  root :to => 'home#index'
-  get 'cirriculum', :to => 'home#cirriculum'
+
+  get 'curriculum', :to => 'home#curriculum'
+  get 'kits', :to => 'home#kits'
 
 end
