498e44a77fe8b6d58e4b464661df31b94173ad0d
hoang.nguyen <hoang.nguyen@techpropulsionlabs.com>
2012-11-27 17:39:20 +0700

    servermail config

--- config/application.rb
+++ config/application.rb
@@ -56,6 +56,7 @@ module Littlespark
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    # config divese with heroku
     config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,4 +35,20 @@ Littlespark::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  # config.action_mailer.default_url_options = { :host => 'littlespark-stg.herokuapp.com' }
+  # ActionMailer Config
+  # Setup for production - deliveries, no errors raised
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+      :address => "smtp.gmail.com",
+      :port => 587,
+      :domain => 'localhost:3000',
+      :user_name => 'tpldev1',
+      :password => 'tpldev1@',
+      :authentication => 'plain',
+      :enable_starttls_auto => true
+  }
 end
