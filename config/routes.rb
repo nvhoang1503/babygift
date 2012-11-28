ee14d9df63b96d45b9d971dd88d0a1b82b95e71a
Phuong Doan <phuong.doan@techpropulsionlabs.com>
2012-11-28 13:55:44 +0700

    [helper] customized_link_to for active link map to  multiple paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Littlespark::Application.routes.draw do
+  get "enrolment/step_1"
+  get "enrolment/step_2"
+  get "enrolment/step_3"
+  get "enrolment/step_4"
+  get "enrolment/step_5"
+  get "enrolment/finish"
+
   devise_for :users
 
   root :to => 'home#index'
