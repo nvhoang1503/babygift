5914fa66e83d0661b8ad974af253fc01b1bd8256
Phuong Doan <phuong.doan@techpropulsionlabs.com>
2012-11-30 16:20:25 +0700

    [enroll] step 3: login & redirect to step 4

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,6 @@
 //= require jquery_ujs
 //= require twitter/bootstrap
 //= require bootstrap-datepicker
+//= require helper
 //= require application
+//= require init
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  def after_sign_in_path_for(resource)
+    if request.env["HTTP_REFERER"] == url_for(:controller => '/enrolment', :action => :step_3, :only_path => false)
+      enrolment_step_4_path
+    else
+      root_path
+    end
+  end
 end
