2c0234b889c9b6468d69ad94b6c98b2116ee4575
Phuong Doan <phuong.doan@techpropulsionlabs.com>
2012-12-05 11:04:39 +0700

    US state collection helper, within enroll step 4

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
     [ enrolment_step_1_path, enrolment_step_2_path, enrolment_step_3_path,
       enrolment_step_4_path, enrolment_step_5_path, enrolment_finish_path ]
   end
+
+  def us_states_collection
+    Country['US'].states.map { |k,v| [k, v['name']]}
+  end
 end
