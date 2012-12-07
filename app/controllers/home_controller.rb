11cc3d6058c14a591e61b6b8d9aba3295331b509
hoang.nguyen <hoang.nguyen@techpropulsionlabs.com>
2012-12-07 14:10:22 +0700

    remove home/test

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -23,10 +23,10 @@ class HomeController < ApplicationController
   def fan_page
   end
 
-  def test
-    user = current_user
-    @response = 111111111
-    UserMailer.enroll_email(user, @response ).deliver
-  end
+  # def test
+  #   user = current_user
+  #   @response = 111111111
+  #   UserMailer.enroll_email(user, @response ).deliver
+  # end
 
 end
