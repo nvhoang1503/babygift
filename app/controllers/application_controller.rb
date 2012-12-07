910658d894a8525931258ff88e268362d8c9cded
Phuong Doan <phuong.doan@techpropulsionlabs.com>
2012-12-07 15:03:19 +0700

    update after sign in path: base on referer path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
   def after_sign_in_path_for(resource)
-    if request.env["HTTP_REFERER"] == url_for(:controller => '/enrolment', :action => :step_3, :only_path => false)
-      enrolment_step_4_path
+    env = Rack::MockRequest.env_for request.referer
+    req_referer = Rack::Request.new env
+    if req_referer.path == url_for(:controller => '/enrolment', :action => :step_3, :only_path => true)
+      enrolment_step_4_path(req_referer.params)
     else
       root_path
     end
