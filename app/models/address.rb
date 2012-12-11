8617ece890a6faf4a171494532bad3cfe24b2cd6
hoang.nguyen <hoang.nguyen@techpropulsionlabs.com>
2012-12-11 14:06:21 +0700

    update missing code

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,6 +1,6 @@
 class Address < ActiveRecord::Base
-  attr_accessible :address_1, :address_2, :city, :first_name, :last_name, :state, :zip
-  validates_presence_of :address_1, :city, :first_name, :last_name, :state, :zip
+  attr_accessible :address_1, :address_2, :city, :first_name, :last_name, :phone, :state, :zip
+  validates_presence_of :address_1, :city, :first_name, :last_name, :phone, :state, :zip
 
   validates :zip, :format => { :with => /^\d{5}(-\d{4})?$/, :message => 'format must be "12345" or "12345-1234"'}
   validates :phone, :format => { :with => /^\d{3}-\d{3}-\d{4}$/, :message => 'format must be "123-123-1234"'}
