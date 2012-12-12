2a28372207f71831b9484797e611adecfa82d1a2
Phuong Doan <phuong.doan@techpropulsionlabs.com>
2012-12-12 15:18:06 +0700

    bug fixed: wrong baby name in order confirm email

--- app/models/baby.rb
+++ app/models/baby.rb
@@ -9,4 +9,8 @@ class Baby < ActiveRecord::Base
   validates :first_name, :last_name,:presence => { :message => "Can't be blank." }
 
   belongs_to :parent, :class_name => 'User', :foreign_key => :user_id
+
+  def fullname
+    [self.first_name, self.last_name].join(' ')
+  end
 end
