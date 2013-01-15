8984b2324dc433689826b8754a781ece3b41c297
hoang.nguyen <hoang.nguyen@techpropulsionlabs.com>
2013-01-15 15:44:13 +0700

    [Gift] add description bellow the plan_type

--- app/helpers/gifts_helper.rb
+++ app/helpers/gifts_helper.rb
@@ -7,25 +7,33 @@ module GiftsHelper
             #{ number_to_currency Order::PRICE[Order::TYPE['1_mon']]
             }
           </div>
-        </div>".html_safe] ,
+        </div>
+        <div class='info clearboth'>Billed monthly,<br> cancel anytime</div>
+        ".html_safe] ,
       [Order::TYPE['3_mon'],
         "<div class='icon-plan _3-month pull-left  #{'selected' if selected== Order::TYPE['3_mon']}'>
           <div class='price'>
             #{ number_to_currency Order::PRICE[Order::TYPE['3_mon']] }
           </div>
-        </div>".html_safe] ,
+        </div>
+        <div class='info clearboth'>Pay upfront and save,<br> renews automatically</div>
+        ".html_safe] ,
       [Order::TYPE['6_mon'],
         "<div class='icon-plan _6-month pull-left  #{'selected' if selected== Order::TYPE['6_mon']}'>
           <div class='price'>
             #{ number_to_currency Order::PRICE[Order::TYPE['6_mon']] }
           </div>
-        </div>".html_safe] ,
+        </div>
+        <div class='info clearboth'>Pay upfront and save,<br> renews automatically</div>
+        ".html_safe] ,
       [Order::TYPE['12_mon'],
         "<div class='icon-plan _12-month pull-left  #{'selected' if selected== Order::TYPE['12_mon']}'>
           <div class='price'>
             #{ number_to_currency Order::PRICE[Order::TYPE['12_mon']] }
           </div>
-        </div>".html_safe]
+        </div>
+        <div class='info clearboth'>Pay upfront and save,<br> renews automatically</div>
+        ".html_safe]
     ]
   end
 end
