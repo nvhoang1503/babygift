55609011dfcfd0c852bcca51c83e74a3b33de07d
Dang Nguyen <dang.nguyen@techpropulsionlabs.com>
2013-01-21 17:04:35 +0700

    update validation for account information

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,17 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
+
+  EMAIL_REG_EXP = /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@(([0-9a-zA-Z])+([-\w]*[0-9a-zA-Z])*\.)+[a-zA-Z]{2,9})$/i
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
   # attr_accessible :email, :password, :password_confirmation, :remember_me
   attr_accessible :first_name, :last_name,:email_notification, :email, :email_confirmation, :password, :password_confirmation, :remember_me
-  validates :email,  :presence => { :message => "Can't be blank" }
+  validates :email, :first_name, :last_name, :presence => { :message => "Can't be blank" }
+  validates :email, :format => {:with => EMAIL_REG_EXP, :message => I18n.t('message.invalid_email')}
   validates :password,  :presence => { :message => "Can't be blank" },:unless => lambda {|u| u.password.nil? }
   validates :password,  :length => {:maximum => 20, :minimum => 6, :message => I18n.t('message.pass_format')},:unless => lambda {|u| u.password.nil? }
 
