1cf8a65bfbc80a41c79d357f8655cd2c6bad5a74
Phuong Doan <phuong.doan@techpropulsionlabs.com>
2013-03-06 14:19:06 +0700

    [bug fixed] allow letter + in gift emails' format

--- app/models/gift.rb
+++ app/models/gift.rb
@@ -1,5 +1,5 @@
 class Gift < ActiveRecord::Base
-  EMAIL_REG_EXP = /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@(([0-9a-zA-Z])+([-\w]*[0-9a-zA-Z])*\.)+[a-zA-Z]{2,9})$/i
+  EMAIL_REG_EXP = /^([0-9a-zA-Z]([-+.\w]{0,1}[0-9a-zA-Z])*@(([0-9a-zA-Z])+([-\w]{0,1}[0-9a-zA-Z])*\.)+[a-zA-Z]{2,9})$/i
 
   attr_accessible :plan_type, :sender_email, :note, :recipient_email, :transaction_status, :transaction_date, :transaction_code, :gift_code
   validates :sender_email, :recipient_email , :presence => {:message => I18n.t('message.not_blank')}, :format => {:with => EMAIL_REG_EXP, :message => I18n.t('message.invalid_email')}
